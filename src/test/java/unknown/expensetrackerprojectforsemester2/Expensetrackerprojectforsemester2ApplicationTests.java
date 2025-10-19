caf9da1cfde6b4ec6d89f6ec82f2ee2eac72413b
kingjosh2 <azubukojoshua@gmail.com>
2025-10-19 22:24:50 +0100

    Initial commit

---
+++ src/test/java/unknown/expensetrackerprojectforsemester2/Expensetrackerprojectforsemester2ApplicationTests.java
@@ -0,0 +1,13 @@
+package unknown.expensetrackerprojectforsemester2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expensetrackerprojectforsemester2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
